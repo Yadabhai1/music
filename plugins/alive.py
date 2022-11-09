314584be150819d5e050f9fba0a822cb21a90da4
SHIYA-XD <116907822+SHIYA-XD@users.noreply.github.com>
2022-11-09 15:15:39 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -51,7 +51,7 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/adityaserver")
+                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/Itz_friends_haveli")
                 ]
                 
            ]
